ef47f027132f850ee3c085b89f6dee1624152192
tylermcgarry <46566991+tylermcgarry@users.noreply.github.com>
2019-04-01 11:51:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
 let canvas = document.getElementById('main');
 let c = canvas.getContext('2d');
 
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+let pos = new Vector(60, 60);
+let vel = new Vector(.5, .2);
+
 function init() {
-	alert('Hello World');
-    update();
+	
+	console.log(pos.toString());
+	update();
 }
 
 function update() {
-
-    requestAnimationFrame(update);
+	
+	c.clearRect(0, 0, canvas.width, canvas.height);
+	
+	pos.addVector(vel);
+	
+	c.beginPath();
+	c.arc(pos.x, pos.y, 50, 0, Math.PI*2, false);
+	c.stroke();
+	
+	
+	requestAnimationFrame(update);
 }
 
 window.addEventListener('load', function(event) {
-    init();
+	init();
 });

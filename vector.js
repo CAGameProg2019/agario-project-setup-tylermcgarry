c7827c314edc2bbb7223e8fd801bdd8a623b56fe
tylermcgarry <46566991+tylermcgarry@users.noreply.github.com>
2019-04-01 11:51:26 -0400

    Update vector.js

--- vector.js
+++ vector.js
@@ -1,7 +1,28 @@
 class Vector {
     
-    constructor() {
-
+    constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		
     }
+	
+	addVector(vec) {
+		this.x += vec.x;
+		this.y += vec.y;
+		return this;
+	}
+	
+	// Functions For HW
+	subVector(vec) { // subtracting vectors
+		
+	}
+	
+	scale(s) { // scale vectors
+		
+	}
+	
+	toString() {
+		return '<'+ this.x+', '+this.y+'>';
+	}
 
 }

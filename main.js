7d27a0066b7b040a46afbab00e35c218fc4ca7c5
tylermcgarry <46566991+tylermcgarry@users.noreply.github.com>
2019-04-07 15:55:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,21 +5,21 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 let colors = [
-  	'#324001', 
-	'#667302', 
-	'#F2B33D', 
-	'#BF4124', 
-	'#730202'	
+  	'#ED553B', 
+	'#F2B134', 
+	'#47AB6C', 
+	'#0894A1', 
+	'#112F41'	
 ];
-
+let foodNum = 100;
 function randomColor () {
 	let index = Math.floor(Math.random()*colors.length)
 	return colors[index];
 }
 
-let pos = new Vector(60, 60); // Starting Location
-let vel = new Vector(1, 1); // Velocity
-vel.scale(2);
+//let pos = new Vector(60, 60); // Starting Location
+//let vel = new Vector(1, 1); // Velocity
+//vel.scale(2);
 
 
 
@@ -27,23 +27,26 @@ let foods = [];
 
 
 function init() {
-	let x = Math.random() * canvas.width;
-	let y = Math.random() * canvas.height;
+	foods = [];
+		for (var i = 0; i < foodNum; i++) {
+			let radius = 10
+		let x = Math.random() * canvas.width;
+		let y = Math.random() * canvas.height;
+
+		let food = new Food(x, y, radius, randomColor())
+		foods.push(food)
+		update();
+	}
 	
-	let food = new Food(x, y, 20, randomColor())
-	foods.push(food)
-	update();
 }
 
 function update() {
 	requestAnimationFrame(update);
 	c.clearRect(0, 0, canvas.width, canvas.height);
-	
-	foods[0].draw(c);
+	for (var i = 0; i < foods.length; i++)
+	foods[i].draw(c);
 	
 
-	
-	
 	
 }
 
@@ -54,4 +57,9 @@ function update() {
 window.addEventListener('load', function(event) {
 	init();
 	
+});
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
 });
